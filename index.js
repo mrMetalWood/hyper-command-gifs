0953fa7d26dfa63187446d401ef20d4d9bc43ab4
Malte Modrow <modrow@ubilabs.net>
2016-12-20 20:34:57 +0100

    feat(query): adjust query parsing

--- index.js
+++ index.js
@@ -2,34 +2,38 @@ const commandMatches = /push/;
 
 exports.middleware = (store) => (next) => (action) => {
   if ('SESSION_ADD_DATA' === action.type) {
-    const {data} = action;
-    const match = commandMatches.exec(data);
-
-    console.log(data);
-
-    if (match != null) {
-      store.dispatch({
-        type: 'DISPLAY_GIF',
-        query: match[0]
-      });
-
-      next(action);
-    } else {
-      store.dispatch({
-        type: 'DISPLAY_GIF',
-        query: ''
-      });
-      next(action);
-    }
-  } else {
-    next(action);
+    store.dispatch({
+      type: 'UPDATE_QUERY',
+      query: action.data
+    });
   }
+
+  next(action);
 };
 
 exports.reduceUI = (state, action) => {
   switch (action.type) {
-    case 'DISPLAY_GIF':
-      return state.set('query', `${action.query}`);
+    case 'UPDATE_QUERY': {
+      const {query} = action;
+
+      if (!state.query) {
+        return state.set('query', ' ');
+      }
+
+      if (query.length <= 1) {
+        return state.set('query', `${state.query}${query}`);
+      }
+
+      if (query.length > 1 && /^[a-zA-Z]*$/.exec(query)) {
+        return state.set('query', query);
+      }
+
+      return state;
+    }
+
+    case 'SET_QUERY': {
+      return state.set('query', action.query);
+    }
   }
   return state;
 };
@@ -56,46 +60,91 @@ exports.decorateTerm = (Term, { React, notify }) => {
   return class extends React.Component {
     constructor (props, context) {
       super(props, context);
-      this._onTerminal = this._onTerminal.bind(this);
+
+      this.state = {gifInProgress: false};
+
+      this.onTerminal = this.onTerminal.bind(this);
+
     }
 
-    componentWillReceiveProps (next) {
-      if (commandMatches.exec(next.query)) {
-        this.showGif(next.query);
+    shouldComponentUpdate(nextProps) {
+      if (nextProps.query && nextProps.query !== this.props.query) {
+        return true;
+      }
+
+      return false;
+    }
+
+
+    showGif() {
+      const match = commandMatches.exec(this.props.query);
+
+      if (match && !this.state.gifInProgress) {
+
+        fetch(
+          `http://api.giphy.com/v1/gifs/search?q=${match[0]}&api_key=dc6zaTOxFJmzC`
+        )
+        .then(response => response.json())
+        .then(gifs => {
+          const {data} = gifs;
+          const gifCount = data.length - 1;
+          const gif = Math.floor(Math.random() * (gifCount - 0 + 1));
+
+          this.div.style.cssText = `
+            background:
+              url('https://media.giphy.com/media/l0NwFpmhycSR8hYdi/200.gif');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+          `;
+
+          this.setState({gifInProgress: true})
+
+          setTimeout(() => this.clearGif(), 5000)
+        });
       }
     }
 
-    showGif(query) {
-      fetch(
-        `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC`
-      )
-      .then(response => response.json())
-      .then(gifs => {
-        const {data} = gifs;
-        const count = data.length;
-        const gif = Math.floor(Math.random() * (count - 0 + 1));
-
-        this.div.style.cssText = `background:
-          url('https://media.giphy.com/media/l0NwFpmhycSR8hYdi/200.gif');
-          background-size: cover;
-          background-repeat: no-repeat`;
-
-        setTimeout(() => {this.div.style.background = '';}, 5000)
-      });
+    clearGif() {
+      this.div.style.background = '';
+      this.setState({gifInProgress: false});
     }
 
-    _onTerminal(term) {
-      if (this.props.onTerminal) this.props._onTerminal(term);
-
-      console.log('term', term);
-
+    onTerminal(term) {
       this.div = term.div_;
+
+      term.document_.addEventListener(
+        'keyup', event => this.handleKeyUp(event),
+        false
+      );
+    }
+
+    handleKeyUp(event) {
+      const {keyCode} = event;
+
+      if(keyCode === 13) {
+        this.showGif();
+
+        store.dispatch({
+          type: 'SET_QUERY',
+          query: ' '
+        });
+      } else if (keyCode === 8) {
+        store.dispatch({
+          type: 'SET_QUERY',
+          query: this.props.query.slice(0, -1)
+        });
+      }
     }
 
     render () {
       return React.createElement(
         Term,
-        Object.assign({}, this.props, {onTerminal: this._onTerminal})
+        Object.assign(
+          {},
+          this.props,
+          {onTerminal: this.onTerminal}
+        )
       );
     }
   }
